706801f6d3b78023b701fe2e21408b0c5d88123e
Kaue Sloboda <kaue.sloboda@targetwork.com.br>
2022-04-14 15:15:12 -0300

    Incluindo o objeto Familia

---
+++ Filme.Domain/Familia/Familia.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filme.Domain.Familia
+{
+    public class Familia
+    {
+        public Guid idFamilia { get; set; }
+        public string Nome { get; set; }
+        public int IdUsuario { get; set; }
+    }
+}
